a35b33ab847d32c61ef975eaafea428b48966177
Carolanne <myridya@hotmail.com>
2019-02-27 12:39:33 -0500

    Commit 1: Animation de header

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,10 @@
 window.addEventListener('load', function(){
     console.log('Oui animation')
+
+    let titre = document.querySelector('.site-branding');
+    let sousTitre = document.querySelector('.site-description');
+
+    sousTitre.classList.add("animSousTitre");
+
+    console.log(sousTitre);
 })
