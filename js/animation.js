d7f46bde7e4a2f01ab3bb4288bdd5fe3baf8ca3e
Carolanne <myridya@hotmail.com>
2019-02-27 13:27:14 -0500

    Commit 3: Animation du sous-titre

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,11 @@
 window.addEventListener('load', function(){
     console.log('Oui animation')
 
-    let titre = document.querySelector('.site-branding');
+    let titre = document.querySelector('.site-title');
     let sousTitre = document.querySelector('.site-description');
 
     sousTitre.classList.add("animSousTitre");
+    titre.classList.add("animTitre");
 
     console.log(sousTitre);
 })
