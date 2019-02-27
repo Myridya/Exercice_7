581bf2ee0ece0ff9c2482a0953e44e0d19da01a4
Carolanne <myridya@hotmail.com>
2019-02-27 14:00:08 -0500

    Commit 5: Animer une classe dans Acceuil

--- js/animation.js
+++ js/animation.js
@@ -3,9 +3,12 @@ window.addEventListener('load', function(){
 
     let titre = document.querySelector('.site-title');
     let sousTitre = document.querySelector('.site-description');
+    let descSite = document.querySelector('.descriptionSite');
 
     sousTitre.classList.add("animSousTitre");
     titre.classList.add("animTitre");
+    descSite.classList.add("animDescSite");
+
 
     console.log(sousTitre);
 })
